4db3ab3870f85ddcdf31860de535121d535e6d7a
Soufiane Majdar <soufianemjr3145@gmail.com>
2023-09-15 18:24:46 +0100

    update

--- get_business_data.py
+++ get_business_data.py
@@ -3,21 +3,23 @@ from lxml import html
 import requests
 import json
 
+# Base url
+base_url = 'https://www.marocannuaire.org/Annuaire/'
 
 ######
 # Xpaths ::
 ####
 ## Business Data xpaths
-name_xpath =''
-image_url_xpath = ''
-category_xpath = ''
-address_xpath = ''
-city_xpath = ''
-phone_xpath = ''
-mobile_xpath = ''
-email_xpath = ''
-website_xpath = ''
-description_xpath = ''
+name_xpath ='//*[@id="content"]/div/div/div/div[1]/h3/text()'
+image_url_xpath = '//*[@id="content"]/div/div/div/div[1]/p[1]/img/@src'
+category_xpath = '//*[@id="content"]/div/div/div/div[1]/u[1]/a/strong/text()'
+address_xpath = '//*[@id="content"]/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/font/ul/li[1]/text()[2]'
+city_xpath = '//*[@id="content"]/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/font/ul/li[1]/text()[4]'
+phone_xpath = '//*[@id="content"]/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/font/ul/li[2]/a[1]/u/text()'
+mobile_xpath = '//*[@id="content"]/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/font/ul/li[2]/a[2]/u/text()'
+email_xpath = '//*[@id="content"]/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/font/ul/a[1]/text()'
+website_xpath = '//*[@id="content"]/div/div/div/div[1]/div/div[1]/div/div/div/div/div[1]/font/ul/li[4]/a/text()'
+description_xpath = '//*[@id="content"]/div/div/div/div[1]/div/div[1]/div/div/div/div/div[2]/div/div/div/div/p[1]/font/text()'
 
 
 
@@ -49,3 +51,49 @@ class Business:
             'website': self.website,
             'description': self.description
         }
+
+
+
+
+# method to get business data
+def get_business_data(url):
+    page = requests.get(base_url + url)
+    tree = html.fromstring(page.content)
+    name = tree.xpath(name_xpath)
+    image_url = tree.xpath(image_url_xpath)
+    category = tree.xpath(category_xpath)
+    address = tree.xpath(address_xpath)
+    city = tree.xpath(city_xpath)
+    phone = tree.xpath(phone_xpath)
+    mobile = tree.xpath(mobile_xpath)
+    email = tree.xpath(email_xpath)
+    website = tree.xpath(website_xpath)
+    description = tree.xpath(description_xpath)
+    return Business(name, image_url, category, address, city, phone, mobile, email, website, description)
+
+
+# method to save business data on file json
+def save_business_data(business):
+    with open('business_data.json', 'a') as outfile:
+        json.dump(business.to_json(), outfile, indent=4)
+        outfile.write(',')
+
+
+# method to get all business data
+def get_all_business_data():
+    with open('business_urls.json') as json_file:
+        urls = json.load(json_file)
+        for url in urls:
+            try:
+                business = get_business_data(url)
+                save_business_data(business)
+            except Exception as e:
+                print(e)
+                print('Error while getting business data')
+
+
+try:
+    get_all_business_data()
+except Exception as e:
+    print(e)
+    print('Error while getting business data')
--- get_business_urls.py
+++ get_business_urls.py
@@ -1,30 +1,13 @@
-
 # Import required modules
 from lxml import html
 import requests
 import json
 
-# Request the page
-# number of the page 
-i=0
-page = requests.get('https://www.marocannuaire.org/Annuaire/activite_ville.php?pageNum_re_aff_dernier_anscri_index={i}&totalRows_re_aff_dernier_anscri_index=80&activite=Restaurants&ville=RABAT%20SALE')
-
-print(page.status_code)
-
-######
-# Xpaths ::
-####
-##  Urls of the business
-url_xpath = '//div[@class="content-nowon-newday"]/h3/a/@href'
-
-
-
-
-
 # method to get all business urls
 def get_business_urls(page):
     tree = html.fromstring(page.content)
-    urls = tree.xpath(url_xpath)
+    # Corrected XPath for URL extraction
+    urls = tree.xpath('//div[@class="content-nowon-newday"]/h3/a/@href')
     return urls
 
 # method to save business urls on file json
@@ -32,18 +15,24 @@ def save_business_urls(urls):
     with open('business_urls.json', 'w') as outfile:
         json.dump(urls, outfile)
 
-
 # get all business urls of all pages
 def get_all_business_urls():
     urls = []
     for i in range(0, 7):
-        page = requests.get('https://www.marocannuaire.org/Annuaire/activite_ville.php?pageNum_re_aff_dernier_anscri_index={i}&totalRows_re_aff_dernier_anscri_index=80&activite=Restaurants&ville=RABAT%20SALE')
-        urls += get_business_urls(page)
+        # Proper string formatting for the URL
+        url = f'https://www.marocannuaire.org/Annuaire/activite_ville.php?pageNum_re_aff_dernier_anscri_index={i}&totalRows_re_aff_dernier_anscri_index=80&activite=Restaurants&ville=RABAT%20SALE'
+        page = requests.get(url)
+        if page.status_code == 200:
+            urls += get_business_urls(page)
+        else:
+            print(f"Failed to fetch URL for page {i}")
+
     return urls
 
 try:
     urls = get_all_business_urls()
     save_business_urls(urls)
+    print('Business URLs saved successfully.')
 except Exception as e:
     print(e)
-    print('Error while getting business urls')
+    print('Error while getting or saving business urls')
